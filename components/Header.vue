3c8929a02aef2db22e009f8e999d72ae18c32f81
Ayumi Takahashi <ayumee528@gmail.com>
2023-05-16 18:18:38 -0700

    Add link to the header navigation

--- components/Header.vue
+++ components/Header.vue
@@ -34,7 +34,55 @@
           <div class="hidden lg:flex lg:items-center">
             <nav class="flex space-x-4 xl:space-x-10">
               <NuxtLink to="/starmap" class="nav-item">2023 Starmap</NuxtLink>
-              <NuxtLink to="/developers" class="nav-item">Developers</NuxtLink>
+
+              <Popover v-slot="{ open }" class="relative">
+                <PopoverButton
+                  :class="[
+                    open
+                      ? 'text-space-cyan-light'
+                      : 'text-white hover:text-space-cyan-light',
+                    'group inline-flex items-center focus:outline-none focus:ring-0 focus:ring-offset-0 font-medium transition',
+                  ]"
+                >
+                  <span>Developers</span>
+                  <ChevronDownIcon
+                    :class="[
+                      open
+                        ? 'text-space-cyan-light rotate-180 transform'
+                        : 'text-gray-200',
+                      'ml-1 h-4 w-4 group-hover:text-space-cyan-light stroke-2',
+                    ]"
+                    aria-hidden="true"
+                  />
+                </PopoverButton>
+                <transition
+                  enter-active-class="transition ease-out duration-200"
+                  enter-from-class="opacity-0 -translate-y-1"
+                  enter-to-class="opacity-100 translate-y-0"
+                  leave-active-class="transition ease-in duration-150"
+                  leave-from-class="opacity-100 translate-y-0"
+                  leave-to-class="opacity-0 -translate-y-1"
+                >
+                  <PopoverPanel
+                    class="bg-space-gray-dark shadow-lg bg-opacity-95 absolute left-1/2 z-10 mt-4 w-screen max-w-max -translate-x-1/2 py-6 px-10 after:bottom-[100%] after:border-t-transparent after:border-b-space-gray-dark after:absolute after:left-1/2 after:-translate-x-1/2 after:border-8 after:border-x-transparent after:opacity-95"
+                  >
+                    <NuxtLink
+                      class="flex justify-between items-center w-36 py-2 text-white hover:underline transition hover:text-space-cyan-lighter whitespace-nowrap"
+                      to="/developers"
+                    >
+                      Get Started
+                      <ArrowRightIcon class="inline-block w-5 h-5" />
+                    </NuxtLink>
+                    <NuxtLink
+                      class="flex justify-between items-center w-36 py-2 text-white hover:underline transition hover:text-space-cyan-lighter whitespace-nowrap"
+                      to="/developers/university"
+                    >
+                      University
+                      <ArrowRightIcon class="inline-block w-5 h-5" />
+                    </NuxtLink>
+                  </PopoverPanel>
+                </transition>
+              </Popover>
 
               <Popover v-slot="{ open }" class="relative">
                 <PopoverButton
@@ -150,7 +198,6 @@
                 </transition>
               </Popover>
 
-              <!-- <NuxtLink to="/community" class="nav-item">Community</NuxtLink> -->
               <NuxtLink to="/japan" class="nav-item">Japan</NuxtLink>
             </nav>
             <div class="ml-6 xl:ml-12">
