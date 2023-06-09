2d692fc5f1bbcfdae00182cb6e7d20920d09070f
Ayumi Takahashi <ayumee528@gmail.com>
2023-06-09 11:51:55 -0700

    add landing page template and lang files

---
+++ pages/astar-2.0.vue
@@ -0,0 +1,79 @@
+<template>
+  <NuxtLayout name="default">
+    <template #space>
+      <div class="space-gradient relative">
+        <img
+          class="fixed z-[3] portrait:h-screen landscape:w-screen object-cover"
+          src="/images/common/space-stars.svg"
+          alt=""
+          width="1728"
+          height="1728"
+        />
+        <div
+          class="min-h-[50vh] sm:min-h-[60vh] flex items-center justify-start"
+        >
+          <div class="max-w-7xl mx-auto px-4 sm:px-6 relative z-10 w-full">
+            <div class="text-center">
+              <h1
+                class="text-4xl sm:text-6xl lg:text-7xl font-extrabold tracking-tight drop-shadow"
+              >
+                {{ $t("vision.title") }}
+              </h1>
+              <!-- <p class="sm:text-xl lg:text-2xl">
+                {{ $t("contact.description") }}
+              </p> -->
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="max-w-7xl mx-auto px-4 sm:px-6 text-center relative z-10">
+        WIP...
+      </div>
+    </template>
+    <template #earth>
+      <Footer />
+    </template>
+  </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+const route = useRoute();
+import { meta } from "@/content/meta";
+const { t } = useI18n();
+const seoTitle = `${t("contact.title")} | ${meta.siteName} - ${t(
+  "meta.tagline"
+)}`;
+const seoDescription = t("contact.description");
+const seoUrl = `${meta.url}${route.fullPath}`;
+const seoImage = `${meta.image}common.png`;
+
+useServerSeoMeta({
+  title: () => seoTitle,
+  description: () => seoDescription,
+  ogTitle: () => seoTitle,
+  ogDescription: () => seoDescription,
+  ogImage: () => seoImage,
+  ogImageUrl: () => seoImage,
+  ogType: () => "website",
+  ogUrl: () => seoUrl,
+  twitterCard: () => "summary_large_image",
+  twitterTitle: () => seoTitle,
+  twitterDescription: () => seoDescription,
+  twitterImage: () => seoImage,
+});
+
+definePageMeta({
+  layout: false,
+});
+</script>
+
+<style lang="postcss" scoped>
+.space-gradient::before {
+  @apply w-full h-full absolute content-[""] z-[0];
+  background: linear-gradient(
+    180deg,
+    rgba(8, 16, 41, 0) 30%,
+    rgba(8, 16, 41, 100) 100%
+  );
+}
+</style>
