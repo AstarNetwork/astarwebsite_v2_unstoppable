14e6c43be3ddd500b56e1fedfa980f0bb12eb807
Ayumi Takahashi <ayumee528@gmail.com>
2023-07-03 09:32:02 -0700

    add a link to the crowdcast

--- content/notification.ts
+++ content/notification.ts
@@ -1,11 +1,10 @@
 export const notification = {
-  external_link: true,
   en: {
-    text: "Startale and Sony Network Communications Form Capital Alliance",
-    url: "https://startale.org/blog/startale-and-sony-network-communications-form-capital-38815",
+    text: "Astar 2.0: Building the Unstoppable. Join us: 19th July 2023",
+    url: "https://www.crowdcast.io/c/astar2",
   },
   ja: {
-    text: "スターテイル・ラボとソニーネットワークコミュニケーションズが資本提携",
-    url: "https://startale.org/blog/38814",
+    text: "Astar 2.0: Building the Unstoppable. 開催日: 2023年7月19日",
+    url: "https://www.crowdcast.io/c/astar2",
   },
 };
