4376b58c84251daf7f05a385cfe4717215eecada
Ayumi Takahashi <ayumee528@gmail.com>
2023-08-04 18:22:42 -0700

    Replace gtag

--- app.vue
+++ app.vue
@@ -1,23 +1,12 @@
 <template>
-  <component is="noscript">
-    <iframe
-      :src="`https://www.googletagmanager.com/ns.html?id=GTM-53GS73F`"
-      height="0"
-      width="0"
-      style="display: none; visibility: hidden"
-    >
-    </iframe>
-  </component>
-
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
 </template>
 
 <script setup lang="ts">
-const { locale } = useI18n();
 useHead(() => ({
-  htmlAttrs: { lang: locale.value },
+  htmlAttrs: { lang: "en" },
   link: [
     {
       rel: "canonical",
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,29 +1,12 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  app: {
-    baseURL: "/",
-    head: {
-      script: [
-        {
-          hid: "gtm",
-          children: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-53GS73F');`,
-          type: "text/javascript",
-        },
-      ],
-    },
-  },
   modules: [
     "@nuxtjs/tailwindcss",
     "@nuxtjs/i18n",
+    "nuxt-gtag"
   ],
-  runtimeConfig: {
-    public: {
-      siteUrl: "https://unstoppable.astar.network",
-    },
+  gtag: {
+    id: 'G-KHKB1Z7D88'
   },
   i18n: {
     baseUrl: "https://unstoppable.astar.network",
